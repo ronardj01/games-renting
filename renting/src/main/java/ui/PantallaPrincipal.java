dd0ef3bd9ada5f1452fc0a3da11c63f671341b6f
Tony Medina <cayapadeveloper@gmail.com>
2024-10-03 23:09:09 +0200

    refactorizado de pantalla principal

--- renting/src/main/java/ui/PantallaPrincipal.java
+++ renting/src/main/java/ui/PantallaPrincipal.java
@@ -1,40 +1,9 @@
-
 package ui;
 
-import java.util.Arrays;
+public abstract class PantallaPrincipal {
 
-/**
- *
- * @author tonym
- */
-public class PantallaPrincipal {
-    private String mPp[];
-
-    public PantallaPrincipal() {
-        
-    }
-    public  String[] pantallaPrincipal(){
-    this.mPp = new String[]{" Bienvenido a su Tienda favorita de Alquiler de Video Juegos",
-            "*** O V E R L O R D ***",
-            "1 ->> Consultar Lista de Video Juegos.",
-            "2 ->> Rentar Video Juegos.",
-            "3 ->> Retornar Video Juegos."};
-    return mPp;
-    }
-    public PantallaPrincipal(String[] mPp) {
-        this.mPp = mPp;
-    }
-
-    public String[] getmPp() {
-        return mPp;
-    }
-
-    public void setmPp(String[] mPp) {
-        this.mPp = mPp;
-    }
-
-    @Override
-    public String toString() {
-        return "PantallaPrincipal{" + "mPp=" + Arrays.toString(mPp) + '}';
+    public String[] getMenuPrincipal() {
+        String[] menu = {"1 ->> Consultar Lista de Video Juegos.", "2 ->> Rentar Video Juegos.", "3 ->> Retornar Video Juegos."};
+        return menu;
     }
 }
