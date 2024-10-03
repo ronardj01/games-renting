ea9f8ca970ad0edc135b11c8acb92eb1f20ad366
ronardj01 <ronardj01@gmail.com>
2024-10-03 21:23:27 +0200

    refactorizar ListaPorConsola

--- renting/src/main/java/ui/ListaPorConsola.java
+++ renting/src/main/java/ui/ListaPorConsola.java
@@ -5,9 +5,10 @@ package ui;
  * @author ronar
  */
 public abstract class ListaPorConsola {
+
     //método
-     public static String[] getMenuPorConsola() {
-    String[] menu = { "1->> Siguiente Página.", "2->> Página Anterior.", "3->> Volver al Menú Anterior."};
-    return menu;
-  }
+    public static String[] getMenu() {
+        String[] menu = {"1->> Siguiente Página.", "2->> Página Anterior.", "3->> Volver al Menú Anterior."};
+        return menu;
+    }
 }
