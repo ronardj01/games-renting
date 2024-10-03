ac0bc8ec2543a0cd003bc441b2c32739a8476f65
Tony Medina <cayapadeveloper@gmail.com>
2024-10-03 13:33:25 +0200

    crear de netbeans y añadir mysql dependencia

---
+++ renting/src/main/java/main/renting/Renting.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main.renting;
+
+/**
+ *
+ * @author tonym
+ */
+public class Renting {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
