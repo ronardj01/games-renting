14bb47f566a2570e50234019c547785f7237e088
Tony Medina <cayapadeveloper@gmail.com>
2024-10-04 11:33:06 +0200

    creada clase ejemplares

---
+++ renting/src/main/java/entidades/Ejemplar.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+/**
+ *
+ * @author tonym
+ */
+public class Ejemplar {
+
+    private int idEjemplar;
+    private String codigo;
+    private int estanteriaC;
+    private int estanteriaF;
+    private boolean rentado;
+    private int idConsola;
+    private int idJuego;
+
+    //CONSTANTE BOOLEAN
+    private static final boolean RENTADO_POR_DEFECTO = false;
+
+    public Ejemplar() {
+        this.rentado = RENTADO_POR_DEFECTO;
+    }
+
+    public Ejemplar(String codigo, int estanteriaC, int estanteriaF, boolean rentado) {
+        this.codigo = codigo;
+        this.estanteriaC = estanteriaC;
+        this.estanteriaF = estanteriaF;
+        this.rentado = RENTADO_POR_DEFECTO;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getEstanteriaC() {
+        return estanteriaC;
+    }
+
+    public void setEstanteriaC(int estanteriaC) {
+        this.estanteriaC = estanteriaC;
+    }
+
+    public int getEstanteriaF() {
+        return estanteriaF;
+    }
+
+    public void setEstanteriaF(int estanteriaF) {
+        this.estanteriaF = estanteriaF;
+    }
+
+    public boolean isRentado() {
+        return rentado;
+    }
+
+    public void setRentado(boolean rentado) {
+        this.rentado = rentado;
+    }
+
+    public int getIdEjemplar() {
+        return idEjemplar;
+    }
+
+    public int getIdConsola() {
+        return idConsola;
+    }
+
+    public int getIdJuego() {
+        return idJuego;
+    }
+
+    @Override
+    public String toString() {
+        return "Ejemplar{" + "idEjemplar=" + idEjemplar + ", codigo=" + codigo + ", estanteriaC=" + estanteriaC + ", estanteriaF=" + estanteriaF + ", rentado=" + rentado + ", idConsola=" + idConsola + ", idJuego=" + idJuego + '}';
+    }
+
+}
--- renting/src/main/java/entidades/Juego.java
+++ renting/src/main/java/entidades/Juego.java
@@ -9,5 +9,42 @@ package entidades;
  * @author tonym
  */
 public class Juego {
-    
+
+    private int idJuego;
+    private String titulo;
+    private String genero;
+
+    public Juego(String titulo, String genero) {
+        this.titulo = titulo;
+        this.genero = genero;
+    }
+
+    public Juego() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getIdJuego() {
+        return idJuego;
+    }
+
+    @Override
+    public String toString() {
+        return "Juego{" + "idJuego=" + idJuego + ", titulo=" + titulo + ", genero=" + genero + '}';
+    }
+
 }
