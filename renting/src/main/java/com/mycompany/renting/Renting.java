88b486b852727dd0e1301d69e628caea1875ee16
Tony Medina <cayapadeveloper@gmail.com>
2024-10-07 13:49:39 +0200

    crear comentario tony

--- renting/src/main/java/com/mycompany/renting/Renting.java
+++ renting/src/main/java/com/mycompany/renting/Renting.java
@@ -9,7 +9,7 @@ public class Renting {
     public static void main(String[] args) {
 
         // CADENAS FORMATEADAS PARA EL NOMBRE DE LA COMPAÑIA Y LOS ENCABEZADOS
-        String companyName = String.format("\n%50s\n", "*** O V E R L O R D ***");
+        String companyName = String.format("\n%50s\n", "*** O V E R L O R D ***");// Esto es un comentario Tony 
         String bienvenida = String.format("%70s\n", "Bienvenido a su Tienda favorita de Alquiler de Video Juegos");
         String encabezadoVistaConsola = String.format("%-30s %-14s Estanteria\n", "Juegos", "Unidades");
         String encabezadoCodigoJuego = String.format("\n%65s", "Favor introducir el código del video juego: over-234p");
