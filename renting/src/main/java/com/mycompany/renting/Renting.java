030ff3f84adfd6844ada68fcc69ebde9a244b865
ronardj01 <ronardj01@gmail.com>
2024-10-09 22:57:16 +0200

    agregar getVistaEjemplares al main)

--- renting/src/main/java/com/mycompany/renting/Renting.java
+++ renting/src/main/java/com/mycompany/renting/Renting.java
@@ -23,64 +23,65 @@ public class Renting {
         //CADENAS FORMATEADAS
         String companyName = String.format("\n%50s\n", "*** " + PantallaPrincipal.COMPANY_NAME + " ***");
 
-        //IMPRIMIR LOGO Y NOMBRE DE LA COMPAÑIA
-        System.out.print(companyName);
-
         //Pantallas
         int opcion;
         String nombre = "";
+
         do {
+            //IMPRIMIR LOGO Y NOMBRE DE LA COMPAÑIA
+            System.out.print(companyName);
+
             //PANTALLA PRINCIPAL
             getMenuMain(PantallaPrincipal.BIENVENIDA, PantallaPrincipal.getMenuPrincipal());
             System.out.print("\nFavor introduzca una opcion para ingresar: ");
             opcion = scanner.nextInt();
 
             switch (opcion) {
+
                 //PRINCIPAL BUSQUEDA
                 case 1 -> {
-                    System.out.println(companyName);
+                    //CONSULTAR LISTA DE VIDEO JUEGOS DISPONIBLES
+                    int opcionBusqueda;
+                    do {
+                        System.out.println(companyName);
 
-                    getMenuMain(PantallaConsultaPrincipal.ENCABEZADO, PantallaConsultaPrincipal.getMenu());
-                    System.out.print("favor introducir una opcion de busqueda: ");
-                    opcion = scanner.nextInt();
-                    scanner.nextLine(); //Limpiar buffer
-                    switch (opcion) {
-                        //BUSQUEDA POR NOMBRE
-                        case 1 -> {
-                            System.out.println(companyName);
+                        getMenuMain(PantallaConsultaPrincipal.ENCABEZADO, PantallaConsultaPrincipal.getMenu());
+                        System.out.print("favor introducir una opcion de busqueda: ");
+                        opcionBusqueda = scanner.nextInt();
+                        scanner.nextLine(); //Limpiar buffer
+                        
+                        switch (opcionBusqueda) {
 
-                            System.out.print("Favor introducir nombre de video Juego: ");
-                            nombre = scanner.nextLine();
-                            //BUSCAR EJEMPLAR POR NOMBRE EN DB
-                            String[] encabezadoArray = PantallaBusquedaNombre.ENCABEZADO_ARRAY;
-                            String busquedaNombre = String.format("\n| %-30s | %-18s | %-15s | %s |",
-                                    encabezadoArray[0], encabezadoArray[1],
-                                    encabezadoArray[2], encabezadoArray[3]);
-                            System.out.print("------------------------------------------------------------------------------------");
-                            System.out.println(busquedaNombre);
-                            System.out.print("------------------------------------------------------------------------------------");
+                            case 1 -> {
+                                //BUSQUEDA POR NOMBRE
+                                int opcionNombre;
+                                do {
+                                    System.out.println(companyName);
+                                    System.out.print("Favor introducir nombre de video Juego: ");
+                                    nombre = scanner.nextLine();
 
-                            //IMPRIMIR DESDE DB LOS EJEMPLARES NO RENTADOS POR NOMBRE
-                            ArrayList<VistaEjemplar> ejemplaresNombre = DbVistaEjemplar.getVistaEjemplares(nombre);
-                            for (int i = 0; i < ejemplaresNombre.size(); i++) {
-                                System.out.printf("\n| %-30s | %-18s | %-15s | %4d     |",
-                                        ejemplaresNombre.get(i).getTitulo(),
-                                        ejemplaresNombre.get(i).getConsola(),
-                                        ejemplaresNombre.get(i).getEstanteria(),
-                                        ejemplaresNombre.get(i).getUnidades());
-                                System.out.print("\n------------------------------------------------------------------------------------");
+                                    //LLamar pantalla Busqueda por nombre
+                                    getListByName(nombre);
+
+                                    System.out.print("Selecione una opción: ");
+                                    opcionNombre = scanner.nextInt();
+                                    scanner.nextLine(); //Limpiar buffer
+
+                                } while (opcionNombre != 2);
                             }
-
-                            getMenuMain(PantallaBusquedaNombre.ENCABEZADO, PantallaBusquedaNombre.getMenu());
-
                         }
-                    }
+                    } while (opcionBusqueda != 0);
                 }
+                case 2 ->
+                    System.out.print("Rentar Video Juego.");
+                case 3 ->
+                    System.out.print("Retornar Video Juego.");
+
             }
 
         } while (opcion != 4);
 
-        System.out.println("Hast luego!!!");
+        System.out.println("Hasta luego!!!");
 
         try {
             miConexion.close();
@@ -91,6 +92,7 @@ public class Renting {
         }
     }
 
+    //METODOS PARA EL MAIN
     public static void getMenuMain(String encabezadoMenu, String[] menu) {
         String encabezado = String.format("\n%70s\n", encabezadoMenu);
         System.out.println(encabezado);
@@ -99,5 +101,29 @@ public class Renting {
         for (String opcion : menu) {
             System.out.println(opcion);
         }
+    }
+
+    public static void getListByName(String nombre) {
+        //BUSCAR EJEMPLAR POR NOMBRE EN DB
+        String[] encabezadoArray = PantallaBusquedaNombre.ENCABEZADO_ARRAY;
+        String busquedaNombre = String.format("\n| %-30s | %-18s | %-15s | %s |",
+                encabezadoArray[0], encabezadoArray[1],
+                encabezadoArray[2], encabezadoArray[3]);
+        System.out.print("------------------------------------------------------------------------------------");
+        System.out.println(busquedaNombre);
+        System.out.print("------------------------------------------------------------------------------------");
+
+        //IMPRIMIR DESDE DB LOS EJEMPLARES NO RENTADOS POR NOMBRE
+        ArrayList<VistaEjemplar> ejemplaresNombre = DbVistaEjemplar.getVistaEjemplares(nombre);
+        for (int i = 0; i < ejemplaresNombre.size(); i++) {
+            System.out.printf("\n| %-30s | %-18s | %-15s | %4d     |",
+                    ejemplaresNombre.get(i).getTitulo(),
+                    ejemplaresNombre.get(i).getConsola(),
+                    ejemplaresNombre.get(i).getEstanteria(),
+                    ejemplaresNombre.get(i).getUnidades());
+            System.out.print("\n------------------------------------------------------------------------------------");
+        }
+
+        getMenuMain(PantallaBusquedaNombre.ENCABEZADO, PantallaBusquedaNombre.getMenu());
     }
 }
--- renting/src/main/java/ui/PantallaBusquedaNombre.java
+++ renting/src/main/java/ui/PantallaBusquedaNombre.java
@@ -6,14 +6,12 @@ package ui;
  */
 public abstract class PantallaBusquedaNombre {
 
-    public static final String[] ENCABEZADO_ARRAY = {"Juegos", "Consola", "Estanteria", "Unidades"};
+    public static final String[] ENCABEZADO_ARRAY = { "Juegos", "Consola", "Estanteria", "Unidades" };
     public static final String ENCABEZADO = "Favor Elegir una opción: ";
+
     public static String[] getMenu() {
-        String[] menu
-                = {"1->> Siguiente Página. ",
-                    "2->> Página Anterior.",
-                    "3->> Hacer Nueva Búsqueda.",
-                    "4->> Volver al Menú Anterior."};
+        String[] menu = { "1->> Hacer nueva Busqueda. ",
+                "2->> Volver al Menú Anterior." };
         return menu;
     }
 }
